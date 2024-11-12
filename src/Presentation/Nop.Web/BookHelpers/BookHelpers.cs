2d2551c67e9a4745fbb1a1e1336cc43d70c27cad
pnrz_ <pnrz_@PNRZ_DELL_02>
2024-11-12 18:48:49 +0800

    remove  headerlink logout , description show less/ show more ,product details specification tab

--- src/Presentation/Nop.Web/BookHelpers/BookHelpers.cs
+++ src/Presentation/Nop.Web/BookHelpers/BookHelpers.cs
@@ -1,34 +1,84 @@
 ﻿// panorazzi
 
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using LinqToDB.Common;
 using Nop.Core.Domain.Catalog;
+using Nop.Data;
 using Nop.Services.Catalog;
+using Nop.Services.Localization;
+using Nop.Web.Factories;
+using Nop.Web.Models.Catalog;
+using Org.BouncyCastle.Asn1.Cms;
 
 namespace Nop.Web.BookHelpers
 {
     public class BookHelpers
     {
         private readonly ISpecificationAttributeService _specificationAttributeService;
+        private readonly ILocalizationService _localizationService;
 
-        public BookHelpers(ISpecificationAttributeService specificationAttributeService)
+        public BookHelpers(
+            ISpecificationAttributeService specificationAttributeService,
+            ILocalizationService localizationService
+            )
         {
             _specificationAttributeService = specificationAttributeService;
+            _localizationService = localizationService;
         }
 
-        public async Task<string> GetTitleAsync(int bookId)
+        public async Task<string> GetProductSpecificationAttributeAsync(int productId, string attributeName)
         {
-            // use sepecification to SpecificationAttribute.Name = Title
-            var sa = await GetSpecificationAttributeOptionByAttributeNameAsync("Title");
+            var psa = await GetSpecificationAttributeOptionByAttributeNameAsync(productId, attributeName);
 
-            if (sa != null)
-                return $"Title: {sa.Name}";
-
+            if (psa != null)
+                return $"<li><strong>{psa.Name} :</strong> {string.Join(";", psa.Values.Select(x => x.ValueRaw))}</li>";
             return "";
         }
 
-        private async Task<SpecificationAttributeOption> GetSpecificationAttributeOptionByAttributeNameAsync(string attributeName)
+        private async Task<ProductSpecificationAttributeModel> GetSpecificationAttributeOptionByAttributeNameAsync(int productId, string attributeName)
         {
-            return null;
+            var productSpecificationAttributes = await _specificationAttributeService.GetProductSpecificationAttributesAsync(
+                   productId, showOnProductPage: true);
+
+            var result = new List<ProductSpecificationAttributeModel>();
+
+            foreach (var psa in productSpecificationAttributes)
+            {
+                var option = await _specificationAttributeService.GetSpecificationAttributeOptionByIdAsync(psa.SpecificationAttributeOptionId);
+
+                var model = result.FirstOrDefault(model => model.Id == option.SpecificationAttributeId);
+                if (model == null)
+                {
+                    var attribute = await _specificationAttributeService.GetSpecificationAttributeByIdAsync(option.SpecificationAttributeId);
+                    model = new ProductSpecificationAttributeModel
+                    {
+                        Id = attribute.Id,
+                        Name = await _localizationService.GetLocalizedAsync(attribute, x => x.Name)
+                    };
+                    result.Add(model);
+                }
+
+                var value = new ProductSpecificationAttributeValueModel
+                {
+                    AttributeTypeId = psa.AttributeTypeId,
+                    ColorSquaresRgb = option.ColorSquaresRgb,
+                    ValueRaw = psa.AttributeType switch
+                    {
+                        SpecificationAttributeType.Option => WebUtility.HtmlEncode(await _localizationService.GetLocalizedAsync(option, x => x.Name)),
+                        SpecificationAttributeType.CustomText => WebUtility.HtmlEncode(await _localizationService.GetLocalizedAsync(psa, x => x.CustomValue)),
+                        SpecificationAttributeType.CustomHtmlText => await _localizationService.GetLocalizedAsync(psa, x => x.CustomValue),
+                        SpecificationAttributeType.Hyperlink => $"<a href='{psa.CustomValue}' target='_blank'>{psa.CustomValue}</a>",
+                        _ => null
+                    }
+                };
+
+                model.Values.Add(value);
+            }
+
+            return result.FirstOrDefault(x => x.Name.ToLowerInvariant() == attributeName.ToLowerInvariant());
         }
     }
 }
