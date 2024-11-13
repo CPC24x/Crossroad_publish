581a4fdf315be959bc6e4c0d1708d863f48d68ad
pnrz_ <pnrz_@PNRZ_DELL_02>
2024-11-13 11:54:29 +0800

    update top menu localization, About the author tab ,top product specification

---
+++ src/Db Changes/localization Update.sql
@@ -0,0 +1,3 @@
+UPDATE localestringresource SET ResourceValue="{0}" WHERE ResourceName ="ShoppingCart.HeaderQuantity";
+UPDATE localestringresource SET ResourceValue="items" WHERE ResourceName ="ShoppingCart";
+UPDATE localestringresource SET ResourceValue="Home" WHERE ResourceName ="HomePage";
