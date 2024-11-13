94636cf35ba6040d451dcc858496de66e5f33043
pnrz_ <pnrz_@PNRZ_DELL_02>
2024-11-13 12:51:13 +0800

    fix menu toogle (mobile)

--- src/Db Changes/localization Update.sql
+++ src/Db Changes/localization Update.sql
@@ -1,3 +1,5 @@
 UPDATE localestringresource SET ResourceValue="{0}" WHERE ResourceName ="ShoppingCart.HeaderQuantity";
 UPDATE localestringresource SET ResourceValue="items" WHERE ResourceName ="ShoppingCart";
 UPDATE localestringresource SET ResourceValue="Home" WHERE ResourceName ="HomePage";
+
+UPDATE localestringresource SET ResourceValue="Keep In Touch With Us" WHERE ResourceName ="footer.followus";
