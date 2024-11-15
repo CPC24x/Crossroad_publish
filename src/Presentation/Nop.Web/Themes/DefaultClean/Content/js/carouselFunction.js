3cd1219a1688d7fe1e2b11d236aeb95680d6e9ef
pnrz_ <pnrz_@PNRZ_DELL_02>
2024-11-15 11:40:08 +0800

    fix homepage slider, set carousel nav size

--- src/Presentation/Nop.Web/Themes/DefaultClean/Content/js/carouselFunction.js
+++ src/Presentation/Nop.Web/Themes/DefaultClean/Content/js/carouselFunction.js
@@ -7,6 +7,10 @@ $(window).on('load', function () {
     loop: true,
     margin: 25,
     nav: true,
+    navText: [
+      "<i class='fa fa-angle-left'></i>",
+      "<i class='fa fa-angle-right'></i>"
+    ],
     dots: false,
     responsive: {
       0: {
@@ -26,6 +30,10 @@ $(window).on('load', function () {
     loop: true,
     margin: 25,
     nav: true,
+    navText: [
+      "<i class='fa fa-angle-left'></i>",
+      "<i class='fa fa-angle-right'></i>"
+    ],
     dots: false,
     responsive: {
       0: {
@@ -66,6 +74,20 @@ $(window).on('load', function () {
       },
       768: {
         items: 3,
+      },
+    }
+  });
+
+  var owl5 = $(".homemain-slider  .owl-carousel");
+  owl5.owlCarousel({
+    stagePadding: 0,
+    margin: 5,
+    loop: true,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
       },
     }
   });
